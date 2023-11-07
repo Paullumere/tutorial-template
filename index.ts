b05529c9cfc089f1375c5bc82b1f0d59d4a11432
Paul lumere <dolezc@gmail.com>
2023-11-08 00:09:03 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import dotenv from "dotenv"
-import { getAccountNonce } from "permissionless"
-import { UserOperation, bundlerActions, getSenderAddress, getUserOperationHash, waitForUserOperationReceipt, GetUserOperationReceiptReturnType } from "permissionless"
-import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
-import { Address, Hash, concat, createClient, createPublicClient, encodeFunctionData, http, Hex } from "viem"
-import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
-import { lineaTestnet, polygonMumbai } from "viem/chains"
-
-console.log("Hello world!")
+// CREATE THE CLIENTS
+const publicClient = createPublicClient({
+  transport: http("https://rpc.goerli.linea.build/"),
+  chain: lineaTestnet
+})
+ 
+const chain = "linea-testnet" // find the list of chain names on the Pimlico verifying paymaster reference page
+const apiKey = "e2d8634c-c611-46d0-b4b8-a26d66465998" // REPLACE THIS
+ 
+const bundlerClient = createClient({
+  transport: http(`https://api.pimlico.io/v1/${chain}/rpc?apikey=${apiKey}`),
+  chain: lineaTestnet
+}).extend(bundlerActions).extend(pimlicoBundlerActions)
+ 
+const paymasterClient = createClient({
+  // ⚠️ using v2 of the API ⚠️ 
+  transport: http(`https://api.pimlico.io/v2/${chain}/rpc?apikey=${apiKey}`),
+  chain: lineaTestnet
+}).extend(pimlicoPaymasterActions)
